21a9df7d4ace5aebdff6304793c57e035d569f8a
AlexandreCunha2001 <alexcunha696@gmail.com>
2020-11-24 11:46:57 +0000

    js

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+let teste = "Xano"
+
+function profissao() {
+    let resultado = 0;
+    let valor = 2;
+    resultado = valor * 2;
+
+    console.log("Profissao");
+    console.log("soma = ", resultado);
+}
+
+    profissao();
+    profissao();
+    profissao();
+    profissao();
+
+    function soma(valor, valor2) {
+        let resultado2 = valor + valor2;
+        console.log("soma = ",resultado2);
+    }
+
+    soma(5, 2);
+    soma(10, 2);
+    soma(5, 9);
