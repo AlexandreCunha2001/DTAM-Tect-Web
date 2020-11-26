058f220995038342632c89d16083ee2d9a2112a8
AlexandreCunha2001 <alexcunha696@gmail.com>
2020-11-26 11:16:55 +0000

    !!!

---
+++ JS/Ficha9/script.js
@@ -0,0 +1,23 @@
+let nome = "Alexandre"
+console.log(nome);
+
+let idade= 19
+console.log(idade);
+
+const apelido = "Cunha"
+console.log(apelido);
+
+nomeCompleto = `${nome} ${apelido}`; 
+console.log(nomeCompleto);
+
+console.log("Estudante ", nomeCompleto); 
+
+let json= {
+    nome: nome,
+    apelido: apelido,
+    idade: idade + 5
+    
+}
+
+console.log(json);
+
